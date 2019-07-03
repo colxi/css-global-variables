ff1cba79a615a691896d40511ea6b929aa01ab1f
colxi <colxi.kl@gmail.com>
2019-07-03 02:31:42 +0200

    v3.0.0
    - code tests
    - transform functions
    - cors handling
    - code refactored
    - module|| head include available
    - MIT licence
    - config Object

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+
 import {CSSGlobalVariables} from '../src/css-global-variables.js';
 
 let output = document.getElementById('output');
@@ -6,54 +7,73 @@ function write(msg){
     let line = document.createElement('div');
     line.innerHTML = msg || '&nbsp';
     output.appendChild(line);
-    console.log(msg || '')
+    console.log(msg || '');
 }
 
 function out(msg){ write(msg) } 
 function warn(msg){ write(msg) } 
 
 
-// TEST 1
-out('- Test : Constructor -> requires "new" keyword')
+// TEST 0
+out('- Test : Constructor -> requires "new" keyword');
 try{ 
     CSSGlobalVariables();
-    warn('FAIL')
- }
-catch(e){ out('PASS')}
-out();
-
-
-// TEST 2
-out('- Test : Configuration -> disable autoPrefix')
-let a = new CSSGlobalVariables( {autoprefix : false} );
-try{ 
-    a.primaryColor = 'red';
-    warn('FAIL')
+    warn('FAIL');
 }
 catch(e){ out('PASS')}
 out();
 
 
+// TEST 1
+out('- Test : Configuration -> disable autoPrefix');
+let t1 = new CSSGlobalVariables( {autoprefix : false} );
+try{ 
+    t1.primaryColor = 'red';
+    warn('FAIL');
+}
+catch(e){ out('PASS') }
+out();
+
+
+// TEST 2
+out('- Test : Configuration -> filter CSS elements (.local-1)');
+let t2 = new CSSGlobalVariables( {filter : '.local-1'} );
+if( Object.keys( t2 ).length !== 3 || !t2.primaryColor ) warn('FAIL (Expecting 3 items )');
+else out('PASS');
+out();
+
+
 // TEST 3
-out('- Test : Configuration -> filter CSS elements (.local-1)')
-let b = new CSSGlobalVariables( {filter : '.local-1'} );
-if( Object.keys( b ).length !== 3 || !b.primaryColor ) warn('FAIL (Expecting 3 items )')
-else out('PASS')
+out('- Test : Configuration -> filter CSS elements (.local-2)');
+let t3 = new CSSGlobalVariables( {filter : '.local-2'} );
+if( Object.keys( t3 ).length !== 1 || !t3.secondaryColor ) warn('FAIL (Expecting 1 item)');
+else out('PASS');
 out();
 
 
 // TEST 4
-out('- Test : Configuration -> filter CSS elements (.local-2)')
-let c = new CSSGlobalVariables( {filter : '.local-2'} );
-if( Object.keys( c ).length !== 1 || !c.secondaryColor ) warn('FAIL (Expecting 1 item)')
-else out('PASS')
+out('- Test : Configuration -> use normalize function (invertCase)');
+let invertCase = n=> n.replace(/\w{1}/g, val=> val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase() ); 
+let t4 = new CSSGlobalVariables( {normalize : invertCase} );
+if( !t4.PRIMARYcOLOR ) warn('FAIL ( t4.PRIMARYcOLOR does not translate to t4.primaryColor )');
+else out('PASS');
 out();
 
 
 // TEST 5
-out('- Test : Configuration -> use normalize function (invertCase)')
-let invertCase = n=> n.replace(/\w{1}/g, val=> val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase() ); 
-let d = new CSSGlobalVariables( {normalize : invertCase} );
-if( !d.PRIMARYcOLOR ) warn('FAIL ( d.PRIMARYcOLOR does not translate to d.primaryColor )')
-else out('PASS')
-out();
+out('- Test : CORS -> Use "crossorigin"="anonymous" attribute');
+new Promise((resolve) => {
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.className= 'remote';
+    link.setAttribute( 'crossorigin', 'anonymous' ) ;
+    link.onload = resolve;
+    link.href = 'https://colxi.info/css-global-variables/test/test-remote.css';
+    document.head.appendChild(link);
+}).then( ()=>{ 
+    let t5 = new CSSGlobalVariables();
+    if( !t5.remoteProp ) warn('FAIL ( cannot find t5.remoteProp )');
+    else out('PASS');
+    console.log(t5)
+});
+
