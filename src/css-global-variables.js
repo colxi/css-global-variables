ce81f5ba249b24e18d311e62063ed176a0c8840f
samk-dev <33263129+samk-dev@users.noreply.github.com>
2023-09-25 05:54:17 +0200

    fix: solves window not defined in ssr context fixes Not SSR friendly #5

--- src/css-global-variables.js
+++ src/css-global-variables.js
@@ -1,18 +1,22 @@
 /*
-*
-* @Package: css-global-variables (CSSGlobalVariables)
-* @Url: https://github.com/colxi/css-global-variables/
-* @Author: colxi
-* @Email: colxi.kl@gmail.com
-* @Date:  2018-03-18
-* @License: MIT
-*
-*/
+ *
+ * @Package: css-global-variables (CSSGlobalVariables)
+ * @Url: https://github.com/colxi/css-global-variables/
+ * @Author: colxi
+ * @Email: colxi.kl@gmail.com
+ * @Date:  2018-03-18
+ * @License: MIT
+ *
+ */
 
-import './main.js';
+import "./main.js";
 
-let _temp = window.CSSGlobalVariables;
+let _temp;
 
-delete window.CSSGlobalVariables;
+if (typeof window !== "undefined") {
+  _temp = window.CSSGlobalVariables;
 
-export {_temp as CSSGlobalVariables};
+  delete window.CSSGlobalVariables;
+}
+
+export { _temp as CSSGlobalVariables };
